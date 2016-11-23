bc18ac79fdf7a75cbcd315c3e78fc11ff3654ff4
Likhi <likhi.ondov@gmail.com>
2016-11-22 18:07:22 -0800

    added description of functions to header of the 22Nov kata
    addeded

--- 22Nov2016.py
+++ 22Nov2016.py
@@ -1,9 +1,9 @@
 '''
 remove_char(s):
-It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter. 
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter.
 
 snail(array):
-
+Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
 '''
 
 def remove_char(s):
@@ -15,37 +15,37 @@ def remove_char(s):
 def snail(array):
   twist = []
   n = len(array)
-  
+
   if array != [[]]: # if the array is not empty...
     for i in range(0,round(n/2)):
       # Add top edge
       twist += array[i][i:n-i-1]
-      
+
       # Add right edge
       for j in range(i,n-i-1):
         twist.append(array[j][n-1-i])
-      
+
       # Add bottom row
       bot_row_forward = array[n-i-1][(i+1):(n-i)]
       bot_row_forward.reverse() #bot_row_backward! ahaaaa...
       twist += bot_row_forward
-      
+
       # Add left edge
       left_edge_forward = []
       for j in range(i+1,n-i):
         left_edge_forward.append(array[j][i])
       left_edge_forward.reverse() #left_edge_backward!
       twist += left_edge_forward
-    
+
     if n%2 > 0: # for an 5x5 or 7x7 or other odd x odd matrix, manually add center value
       twist.append(array[int((n-1)/2)][int((n-1)/2)])
-    
+
     return twist
-  
+
   else: # the array is empty!
       return [] # this is the expected response for a 0x0 empty array, i.e. array = [[]]
-  
-      
+
+
 print(remove_char('hello')) # 'ell'
 print(remove_char('sos'))   # 'o'
 print(remove_char('ho'))    # ''
